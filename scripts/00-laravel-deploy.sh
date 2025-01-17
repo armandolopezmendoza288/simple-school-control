83050ad5666603d9b40877767ae13dc9ddcad3b3
armandolopezmendoza7 <up190316@alumnos.upa.edu.mx>
2025-01-17 10:40:21 -0600

    modify docker configurations 2

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-echo "Running composer"
+echo "Running dump-autoload"
 composer dump-autoload
+
+echo "Running optimize-autoloader --no-dev"
+composer install --optimize-autoloader --no-dev
+
+echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+
+echo "Running composer update"
 composer update
 
 echo "Caching config..."
